8ff1f3224cfc0d721dd7636529c3320db33e19b4
mishafominih <53509997+mishafominih@users.noreply.github.com>
2021-10-13 22:56:53 +0500

    Revert "test"
    This reverts commit 7ef5e742f9daecb03ba3194baaeb38e673e304ac.

--- Lucky/Program.cs
+++ Lucky/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
